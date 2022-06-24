5ef342537b9e830bf32c107e7b5b3f49ba11c4c0
Azer Masimov <mesimovazer@gmail.com>
2022-06-25 03:24:51 +0400

    added h1 title

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    state = { term: "", };
+  state = { term: "" };
 
-    handleFormSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.state.term);
-        this.props.onSubmit(this.state.term);
-    };
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
+  };
 
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form" onSubmit={this.handleFormSubmit}>
-                    <input 
-                        value={this.state.term}
-                        onChange={ (e) => 
-                            this.setState({ term: e.target.value}) 
-                        }
-                    />
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui segment">
+        <h1 style={{ textAlign: "center" }}>Search Pictures</h1>
+        <form className="ui form" onSubmit={this.handleFormSubmit}>
+          <input
+            value={this.state.term}
+            onChange={(e) => this.setState({ term: e.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
